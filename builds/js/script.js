e80f983045f9b1ea2e2101bf2dd5ea6238b5bcdc
StevenS125 <sts12589@gmail.com>
2018-03-06 09:51:14 -0500

    putting finishng touches on project

--- builds/js/script.js
+++ builds/js/script.js
@@ -1,1 +1,15 @@
 var apikey = "AIzaSyC3OCGB-0aKZqu3bwSBaHCmNnvakMGbZ3M"
+
+// function to generate map and location in connect area
+
+function initMap() {
+    var uluru = {lat: 35.227165, lng: -80.846342};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 18,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+  }
